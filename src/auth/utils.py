a4f36557325ac2dfa8716799a5285a067f7858a4
Jahid Hasan <jahidhasan@aihcon.com>
2025-01-06 01:42:21 +0600

    chore: learning jwt authentication

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,8 +1,13 @@
 from passlib.context import CryptContext
+from datetime import datetime,timedelta
+from src.config import Config
+import jwt
+import uuid
 
 passwd_context = CryptContext(
     schemes=['bcrypt']
 )
+ACCESS_TOKEN_EXPIRY = 3600
 
 def generate_passwd_hash(password: str) -> str:
     hash = passwd_context.hash(password)
@@ -12,4 +17,18 @@ def generate_passwd_hash(password: str) -> str:
 def verify_password(password: str, hash: str) -> bool:
     return passwd_context.verify(password, hash)
 
+def create_access_token(user_data: dict, expiry: timedelta= None, refresh: bool = False):
+    payload = {}
 
+    payload['user'] = user_data
+    payload['exp'] = datetime.now() + expiry if expiry is not None else timedelta(seconds=ACCESS_TOKEN_EXPIRY)
+    payload['jti'] = str(uuid.uuid4())
+
+    payload['refresh'] = refresh
+
+    token = jwt.encode(
+        payload=payload,
+        key=Config.JWT_SECRET,
+        algorithm=Config.JWT_ALGORITHM
+    )
+    return token
