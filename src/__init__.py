9621257ec39f1bf3d24132fae7089da542b539a3
Jahid Hasan <jahidhasan@aihcon.com>
2025-01-24 02:12:39 +0600

    chore: learn about fastapi middleware

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,7 @@ from src.reviews.routes import review_router
 from src.tags.routes import tags_router
 from src.db.main import init_db
 from .errors import register_all_errors
+from .middleware import register_middleware
 
 
 
@@ -43,6 +44,7 @@ async def on_startup():
     await init_db()
 
 register_all_errors(app)
+register_middleware(app)
 
 app.include_router(book_router, prefix=f"{version_prefix}/books", tags=["books"])
 app.include_router(auth_router, prefix=f"{version_prefix}/auth", tags=["auth"])
