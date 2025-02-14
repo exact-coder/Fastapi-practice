54d72562ac3938db8704509dae874f19bba01dc8
Jahid Hasan <jahidhasan@aihcon.com>
2025-02-14 14:37:34 +0600

    learn pytest and schemathesis test

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,4 +1,5 @@
 from src.db.main import get_session
+from src.auth.dependencies import AccessTokenBearer, RoleChecker, RefreshTokenBearer
 from src import app
 from fastapi.testclient import TestClient
 from unittest.mock import Mock
@@ -7,11 +8,19 @@ import pytest
 
 mock_session = Mock()
 mock_user_service = Mock()
+mock_book_service = Mock()
+
+access_token_bearer = AccessTokenBearer()
+refresh_token_bearer = RefreshTokenBearer()
+role_checker = RoleChecker(["admin"])
 
 def get_mock_session():
     yield mock_session
 
 app.dependency_overrides[get_session] = get_mock_session
+app.dependency_overrides[role_checker] = Mock()
+app.dependency_overrides[access_token_bearer] = Mock()
+app.dependency_overrides[refresh_token_bearer] = Mock()
 
 @pytest.fixture
 def fake_session():
@@ -24,3 +33,7 @@ def fake_user_service():
 @pytest.fixture
 def test_client():
     return TestClient(app)
+
+@pytest.fixture
+def fake_book_service():
+    return mock_book_service
